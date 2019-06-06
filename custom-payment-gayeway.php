782fda8963fa9898516b524acc325baad62c11cf
Shirkit <shirkit@gmail.com>
2019-06-06 19:26:23 -0300

    Go now

--- custom-payment-gayeway.php
+++ custom-payment-gayeway.php
@@ -12,63 +12,7 @@
 
 add_action( 'plugins_loaded', 'init_your_gateway_class' );
 function init_your_gateway_class() {
-    class WC_Gateway_Your_Gateway extends WC_Payment_Gateway {
-		
-		public function __construct() {
-			$this->id = 'orquidario_cheque';
-			//$this->icon
-			$this->has_fields = false;
-			$this->method_title = 'Cheque Próprio';
-			$this->method_description = 'Forma de pagamento em Cheque';
-			$this->init_form_fields();
-			$this->init_settings();
-			$this->title = $this->get_option( 'title' );
-			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		}
-		
-		public function init_form_fields() {
-			$this->form_fields = array(
-				'enabled' => array(
-					'title' => __( 'Enable/Disable', 'woocommerce' ),
-					'type' => 'checkbox',
-					'label' => __( 'Enable Cheque Payment', 'woocommerce' ),
-					'default' => 'yes'
-				),
-				'title' => array(
-					'title' => __( 'Title', 'woocommerce' ),
-					'type' => 'text',
-					'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-					'default' => __( 'Cheque Payment', 'woocommerce' ),
-					'desc_tip'      => true,
-				),
-				'description' => array(
-					'title' => __( 'Customer Message', 'woocommerce' ),
-					'type' => 'textarea',
-					'default' => ''
-				)
-			);
-		}
-		
-		public function process_payment( $order_id ) {
-			global $woocommerce;
-			$order = new WC_Order( $order_id );
-
-			// Mark as on-hold (we're awaiting the cheque)
-			$order->update_status('on-hold', __( 'Awaiting cheque payment', 'woocommerce' ));
-
-			// Reduce stock levels
-			$order->reduce_order_stock();
-
-			// Remove cart
-			$woocommerce->cart->empty_cart();
-
-			// Return thankyou redirect
-			return array(
-				'result' => 'success',
-				'redirect' => $this->get_return_url( $order )
-			);
-		}
-	}
+        include_once('class-cheque-gateway.php');
 }
 
 function add_your_gateway_class( $methods ) {
